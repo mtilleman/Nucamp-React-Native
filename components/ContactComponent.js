2119d91aee20225b35ed424f7a174fa028eee9aa
mtilleman <michaeltilleman@gmail.com>
2021-09-30 21:07:40 -0500

    Add Contact & About Components

---
+++ components/ContactComponent.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Text, ScrollView } from 'react-native';
+import { Card, Title } from 'react-native-elements';
+
+
+class Contact extends Component {
+
+    static navigationOptions = {
+        title: 'Contact'
+    }
+
+    render() {
+        return (
+            <ScrollView>
+            <Card wrapperStyle={{margin: 20}}
+            title="Contact Information"
+            >
+                <Text>1 Nucamp Way</Text>
+                <Text>Seattle, WA 98001</Text>
+                <Text style={{marginBottom: 10}}>U.S.A.</Text>
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text style={{marginBottom: 10}}>Email: campsites@nucamp.co</Text>
+            </Card>
+            </ScrollView>
+        );
+    }
+}
+
+
+    export default Contact;
